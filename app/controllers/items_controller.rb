abb53a0b0ad62a85024dbdd22cb5748d09138fb2
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-08-15 17:45:55 -0700

    Sorting, required input fields, delete button added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,26 +2,30 @@ class ItemsController < ApplicationController
 
 	before_action :authenticate_user!
 
+  include ItemsHelper
+
   def index
-  	 @items = Item.order("created_at DESC").limit(5)
+  	 @items = current_user.items.limit(25)
   	 @item = Item.new
   end
 
-  def create
-    @item = Item.create(item_params)
-    @item.happines_index = (item_params[:price].to_d + item_params[:happiness].to_d)
-    @item.save!
+  def show
+    @items = current_user.items.limit(25)
+    @item = Item.new
+  end
 
-    if @item.valid?
-      flash[:success] = "Your item was shared"
-      redirect_to :action => :index and return
-    else
-      flash[:error] = "Your item could not be saved"
-    end
+  def create
+    @item = current_user.items.create(item_params)
+    save_if_valid(item_params)
   end
 
   def item_params
     params.require(:item).permit(:title, :price, :happiness)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
   end
 
 end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,37 @@
 module ItemsHelper
+
+	def calculate_h_index(price, happiness)
+		happiness/price
+	end
+
+	def save_if_valid(item_params)
+
+		@title = item_params[:title]
+		@price = item_params[:price]
+		@happiness = item_params[:happiness]
+
+		if ( @title == '')
+	      flash[:notice] = "Please enter a title"
+	      redirect_to :action => :index and return
+	    elsif (@price == '') 
+	      flash[:notice] = "Please enter a valid price"
+	      redirect_to :action => :index and return
+	    elsif (@happiness == '')
+	      flash[:notice] = "Please enter a valid happiness level"
+	      redirect_to :action => :index and return
+	    else
+	      @item.happines_index = calculate_h_index(@price.to_d, @happiness.to_d)
+	      @item.save!
+
+	      #if @item.valid?
+	      flash[:notice] = "Your item was created"
+	      redirect_to :action => :index and return
+	    end
+	end
+
+	def button_to_delete_item(item)
+		item.destroy
+	    redirect_to '/items/index', :notice => "Your item has been deleted"
+	end
+
 end
